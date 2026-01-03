17d9fb6afd73d1bbe92a3fc7997c81384585edc5
Ashish Bhushan Kumar <abhushan064@gmail.com>
2026-01-03 13:33:30 +0530

    refactor(structure): reorder sections to match logical numbering flow

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -17,9 +17,9 @@ export default function Home() {
   return (
     <>
       <Hero />
+      <About />
       <Projects />
       <Skills />
-      <About />
       <Writing />
       <Contact />
     </>
